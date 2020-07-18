40cbd8a87cca3e46b4b97c8c635f9fe98e71c6e1
ntgeek <2584172930@qq.com>
2020-07-18 11:29:22 +0800

    some styles

--- preview/app.js
+++ preview/app.js
@@ -1,5 +1,7 @@
 import React from 'react'
 import ReactDom from 'react-dom'
 import FileUploader from '../src'
-
-ReactDom.render(<FileUploader />, document.getElementById('app'))
+const props = {
+    hint: '上传文件大小不要超过5M'
+}
+ReactDom.render(<div style={{ width: "50vw", height: "90vh", position: "relative" }}> <FileUploader {...props} /></div >, document.getElementById('app'))
--- src/index.tsx
+++ src/index.tsx
@@ -1,11 +1,36 @@
-
 import React, { useState, useEffect } from 'react';
+const styles = require('./index.scss');
 let Uploader = function (props) {
     const [upload_progress, setUpload_progress] = useState(0);//upload progress
     useEffect(() => {
         console.log('use effect!')
     });
-    return <div>上传进度: {upload_progress}%</div>;
+    return (
+        <div>
+
+            <div className={styles.uploader_container}>
+                <input className={styles.uploader_clear_style_of_input} type="file"></input>
+                <div className={styles.uploader_text_hint}></div>
+
+                <div className={styles.uploader_input_file}></div>
+
+                <div className={styles.uploader_icon_outBox}>
+                    <i className={`${styles.uploader_icon} iconfont iconshangchuanwenjian`}></i>
+                </div>
+                <p className={styles.uploader_text_hint}>{props.hint}</p>
+
+                <div className={styles.uploader_progress_outBox}>
+                    <p>上传进度: </p>
+                    <div className={styles.uploader_progress_container}>
+                        <div className={styles.uploader_progress_color}></div>
+                    </div>
+                    <p className={styles.uploader_progress_number}>{upload_progress}%</p>
+                </div>
+            </div >
+
+        </div>
+    );
 }
 
 export default Uploader
+
